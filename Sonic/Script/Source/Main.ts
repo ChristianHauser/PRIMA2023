e5b440959262cac10712d3a622fe1816adc0f5ce
Christian <mr.chris.chris1@gmail.com>
2023-03-24 01:33:21 +0100

    o

--- Sonic/Script/Source/Main.ts
+++ Sonic/Script/Source/Main.ts
@@ -3,17 +3,19 @@ namespace Script {
   ƒ.Debug.info("Main Program Template running!");
 
   let viewport: ƒ.Viewport;
+  let sonic: ƒ.Node  = viewport.getBranch().getChildrenByName("SonicSprite")[0];
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
-
+    //sonic.mtxLocal.translateX(0);
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
 
   function update(_event: Event): void {
     // ƒ.Physics.simulate();  // if physics is included and used
+    sonic.mtxLocal.translateX(0.1);
     viewport.draw();
     ƒ.AudioManager.default.update();
   }
